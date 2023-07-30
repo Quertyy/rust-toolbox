eab2326894355978f20b52e586cd7fe1bd2abbd8
Quertyy <qwertyense@protonmail.com>
2023-07-30 20:00:25 +0200

    refactor code

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,6 +1,62 @@
-use kvs::{Cli, Commands};
-use clap::Parser;
-use std::io::{Write, stderr};
+use clap::{command, Args, Parser, Subcommand};
+use std::io::{stderr, Write};
+
+/// Cli struct is used to parse the command line arguments
+/// It's using the clap crate to do this (https:///docs.rs/clap/latest/clap/)
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+pub struct Cli {
+    /// The subcommand to run
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+/// Commands enum is used to parse the subcommands
+/// Commands are get, set, and rm
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Get subcommand
+    #[command(name = "get")]
+    Get(Get),
+    /// Set subcommand
+    #[command(name = "set")]
+    /// Set struct is defined below
+    Set(Set),
+    /// Remove subcommand
+    #[command(name = "rm")]
+    Remove(Remove),
+}
+
+/// Get struct is used to parse the get subcommand
+/// It has one field, key, which is the key to get
+/// Example: kvs get key
+#[derive(Args, Debug)]
+pub struct Get {
+    /// The key to get
+    pub key: String,
+}
+
+/// Set struct is used to parse the set subcommand
+/// It has two fields, key and value, which are the key and value to set
+/// Example: kvs set key value
+#[derive(Args, Debug)]
+pub struct Set {
+    /// The key to set
+    pub key: String,
+    /// The value to set
+    pub value: String,
+}
+
+/// Remove struct is used to parse the rm subcommand
+/// It has one field, key, which is the key to remove
+/// Example: kvs rm key
+#[derive(Args, Debug)]
+pub struct Remove {
+    /// The key to remove
+    pub key: String,
+}
+
+
 
 fn main() {
     let cli = Cli::parse();
--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,5 @@
-#![deny(missing_docs)]
 //! # kvs
-use std::collections::HashMap; 
-use clap::{Subcommand, Parser, Args, command};
-
-
-/// Cli struct is used to parse the command line arguments
-/// It's using the clap crate to do this (https:///docs.rs/clap/latest/clap/) 
-#[derive(Debug, Parser)]
-#[command(author, version, about)]
-pub struct Cli {
-    /// The subcommand to run
-    #[command(subcommand)]
-    pub command: Commands
-}
-
-/// Commands enum is used to parse the subcommands
-/// Commands are get, set, and rm
-#[derive(Debug, Subcommand)]
-pub enum Commands {
-    /// Get subcommand
-    #[command(name = "get")]
-    Get(Get),
-    /// Set subcommand
-    #[command(name = "set")]
-    /// Set struct is defined below
-    Set(Set),
-    /// Remove subcommand
-    #[command(name = "rm")]
-    Remove(Remove),
-}
-
-/// Get struct is used to parse the get subcommand
-/// It has one field, key, which is the key to get
-/// Example: kvs get key
-#[derive(Args, Debug)]
-pub struct Get {
-    /// The key to get
-    pub key: String
-}
-
-/// Set struct is used to parse the set subcommand
-/// It has two fields, key and value, which are the key and value to set
-/// Example: kvs set key value
-#[derive(Args, Debug)]
-pub struct Set {
-    /// The key to set
-    pub key: String,
-    /// The value to set
-    pub value: String
-}
-
-
-/// Remove struct is used to parse the rm subcommand
-/// It has one field, key, which is the key to remove
-/// Example: kvs rm key
-#[derive(Args, Debug)]
-pub struct Remove {
-    /// The key to remove
-    pub key: String
-}
+use std::collections::HashMap;
 
 /// KvStore struct is used to store key value pairs using a HashMap
 /// It has one field, items, which is the HashMap
@@ -89,5 +30,11 @@ impl KvStore {
     /// Removes a key from the KvStore
     pub fn remove(&mut self, key: String) {
         self.items.remove(&key);
+    }
+}
+
+impl Default for KvStore {
+    fn default() -> Self {
+        Self::new()
     }
 }
