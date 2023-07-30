4d1194c8cf00415a609d387aa6978e857a3a1f42
Quertyy <qwertyense@protonmail.com>
2023-07-30 18:21:03 +0200

    project initialization

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rust Toolbox 
+Project implementation from [talent-plan](https://github.com/pingcap/talent-plan/blob/master/courses/rust/projects/project-1/README.md)
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {}
+
+    pub fn get(&self, key: String) -> Option<String> {
+        None
+    }
+
+    pub fn remove(&mut self, key: String) {}
+}
