ae0b6355b8ea0297f49b2c5811aa638659482d4f
Quertyy <qwertyense@protonmail.com>
2023-07-30 18:31:04 +0200

    feat: finish store

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+pub struct KvStore {
+    pub items: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            items: HashMap::new(),
+        }
     }
 
-    pub fn set(&mut self, key: String, value: String) {}
+    pub fn set(&mut self, key: String, value: String) {
+        self.items.insert(key, value);
+    }
 
     pub fn get(&self, key: String) -> Option<String> {
-        None
+        self.items.get(&key).cloned()
     }
 
-    pub fn remove(&mut self, key: String) {}
+    pub fn remove(&mut self, key: String) {
+        self.items.remove(&key);
+    }
 }
